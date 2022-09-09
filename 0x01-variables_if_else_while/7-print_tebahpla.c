661e62b442f96817b949e1956ba848438500556d
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 16:28:02 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/* A program that prints the lowercase alphabet
- * in reverse
- * followed by a new line.
- */
+/* A program that prints the lowercase alphabet*/
+/* in reverse*/
+/* followed by a new line.*/
 
 /**
  * main - Entry point
