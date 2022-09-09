9659ac9fae28658eda42aa43a1499d4f6c211f6a
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 16:10:29 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/*A program that prints all single digit numbers of base 10 starting from 0, followed by a new line*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i = <58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n);
+
+	return (0);
+}
