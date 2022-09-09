12cf0553132e2696c02730ab21ec339a59db9eea
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 15:57:56 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* A program that prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success0
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
