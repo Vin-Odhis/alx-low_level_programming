7d555a287aca0b5f89c1c8e30f8ec81e0d3dfc3b
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 14:21:13 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'A';
+	while (l <= 'Z')
+	{putchar(1);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
