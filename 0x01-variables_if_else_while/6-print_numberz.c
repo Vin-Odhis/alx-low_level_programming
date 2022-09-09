5e592f9bdbe91f1370e90389e4b7aa038ab03343
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 16:17:44 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 
-/*A program that prints all single digit numbers of base 10 starting from 0,*//* followed by a new line*/
+/* A program that prints all single digit numbers*/
+/* of base 10 starting from 0,*/
+/* followed by a new line*/
 
 /**
  * main - Entry point
