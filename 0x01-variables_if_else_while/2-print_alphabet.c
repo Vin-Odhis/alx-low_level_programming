945a030d1c8346f859d84199592327c4617e2854
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-09 14:14:32 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(1);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
