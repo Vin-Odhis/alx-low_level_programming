c6356aee3f1025567615699a8d442562896e58ea
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-13 16:42:13 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jach_bauer - func that prints every minute of the day of Jack Bauer, starting
  * from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins
+ * and resets mins.
  *
  * Return: 0
  */
