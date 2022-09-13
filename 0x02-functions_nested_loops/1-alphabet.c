17626de703d63a2457597d81ce200c8beab23d00
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-13 15:21:19 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,11 @@
 
 /**
  *print_alphabet - function to ptint abc
- *
  *main - a program that prints the alphabet
  *in lowercase
  *followed by a new line
- *
  *Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char c;
