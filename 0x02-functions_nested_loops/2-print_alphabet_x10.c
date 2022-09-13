bec25df69fd80f78e402d9369f8c726d5d0efa33
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-13 15:29:58 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- print_alphabet_x10 - a function that prints ten
+ *print_alphabet_x10 - a function that prints 10
  *times the alphabet in lowercase
  *followed by a nwe line
  *
