52d506c291adb24b6b994bd8e06b3323d69f76b6
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-22 14:12:35 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,24 +8,15 @@
  * Return: returns new concatenated string
  */
 
-char *_strncat(char *dest, char *src, int n))
+char *_strncat(char *dest, char *src, int n)
 {
-	int a, b;
+	int index = 0, dest_len = 0;
 
-	a = 0;
-	b = 0;
+	while (dest[index++])
+		dest_len++;
 
-
-	while (dest[a] != '\0')
-		a++;
-
-	while (src[b] != '\0' && b < n)
-	{
-		dest[a] = src[b];
-		b++;
-		a++;
-	}
-	dest[a] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
