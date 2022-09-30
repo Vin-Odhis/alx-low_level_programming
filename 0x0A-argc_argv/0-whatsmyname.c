e59ae316e020086a78c59a3b33129c403304ad19
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-09-30 22:28:04 +0300

    Task o

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its own name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
