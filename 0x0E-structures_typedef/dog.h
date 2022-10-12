a344e56615983bed163f36c1b40e6ae08f72e9c6
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-10-13 00:23:28 +0300

    Someone To You - BANNERS

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,35 +1,23 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
 
 /**
- * struct dog - a dogs details
- *
- * @name: first member
- * @age: second member
- * @owner: third member
- *
- * Description: Longer description
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
-};
-
-
-/**
- *dog_t -  typedef for struct dog
- */
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
