c05ef39071b8bc284db29b71a75ee93113fa339a
Vincent Ochieng' Odhiambo <vinodhiambo96@gmail.com>
2022-10-15 12:44:33 +0300

    Damn It Feels Good To Be Me - Andy Grammer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,7 +6,6 @@
  * @size: size of the array.
  * @cmp: pointer to the function to be used
  * to compare values.
- *
  * Return: index of the first eement for which the cmp
  * function does not return 0. If no elements matches,
  * return -1. If size <= 0, return -1.
@@ -18,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		if size <= 0)
+		if (size <= 0)
 			return (-1);
 
 		for (i = 0; i < size; i++)
